930e033905a018f5f8c9776e1d1f91a986df7b75
gaboliveiradev <g38789138@gmail.com>
2022-11-23 14:53:39 -0300

    criando a model de clientes e suas respectivas propriedades

---
+++ Models/ClienteModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _211074.Models
+{
+    public class ClienteModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int id_cidade { get; set; }
+        public DateTime data_nasc { get; set; }
+        public double renda { get; set; }
+        public string cpf { get; set; }
+        public string foto { get; set; }
+        public bool venda { get; set; }
+    }
+}
