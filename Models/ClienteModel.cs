44b46e4849ae3fd11c060ebadb8264097c299bd0
gaboliveiradev <g38789138@gmail.com>
2022-11-23 15:00:26 -0300

    build do metodo incluir na ClienteModel

--- Models/ClienteModel.cs
+++ Models/ClienteModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace _211074.Models
 {
@@ -16,5 +19,31 @@ namespace _211074.Models
         public string cpf { get; set; }
         public string foto { get; set; }
         public bool venda { get; set; }
+
+        public void Incluir()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.comando = new MySqlCommand("INSERT INTO clientes (nome, id_cidade, data_nasc, " +
+                "renda, cpf, foto, venda) VALUES (@nome, @id_cidade, @data_nasc, @renda, @cpf," +
+                "@foto, @venda)", Banco.conexao);
+
+                Banco.comando.Parameters.AddWithValue("@nome", nome);
+                Banco.comando.Parameters.AddWithValue("@id_cidade", id_cidade);
+                Banco.comando.Parameters.AddWithValue("@data_nasc", data_nasc);
+                Banco.comando.Parameters.AddWithValue("@renda", renda);
+                Banco.comando.Parameters.AddWithValue("@cpf", cpf);
+                Banco.comando.Parameters.AddWithValue("@foto", foto);
+                Banco.comando.Parameters.AddWithValue("@venda", venda);
+                Banco.comando.ExecuteNonQuery();
+
+                Banco.FecharConexao();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
